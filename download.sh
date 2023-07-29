d904f1c4ea8f70b00ee89a47a69a6149ad93a682
Koushik Dutta <koushd@gmail.com>
2023-07-28 21:16:15 -0700

    models

---
+++ download.sh
@@ -0,0 +1,9 @@
+for f in ssdlite_mobiledet_coco_qat_postprocess efficientdet_lite0_320_ptq efficientdet_lite1_384_ptq efficientdet_lite2_448_ptq efficientdet_lite3_512_ptq efficientdet_lite3x_640_ptq
+do
+    mkdir -p $f
+    pushd $f
+    BASE=https://raw.githubusercontent.com/google-coral/test_data/master/$f
+    curl -O -L $BASE.tflite
+    curl -O -L "$BASE"_edgetpu.tflite
+    popd
+done
